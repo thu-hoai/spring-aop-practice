ad8ac360d44ca714d0162d1fd7bfd5d4407b1c5d
thu-hoai <hoai.kt178@gmail.com>
2022-07-26 07:51:23 +0700

    Add aspectJ style Spring implementation

--- demo/src/main/java/com/example/demo/aop/methodSecureAspectJStyle/annotation/CustomPreAuthorize.java
+++ demo/src/main/java/com/example/demo/aop/methodSecureAspectJStyle/annotation/CustomPreAuthorize.java
@@ -1,2 +1,14 @@
-package com.example.demo.aop.methodSecureAspectJStyle.annotation;public class CustomPreAuthorize {
-}
+package com.example.demo.aop.methodSecureAspectJStyle.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+public @interface CustomPreAuthorize {}
--- demo/src/main/java/com/example/demo/aop/methodSecureAspectJStyle/config/SecureAspectConfig.java
+++ demo/src/main/java/com/example/demo/aop/methodSecureAspectJStyle/config/SecureAspectConfig.java
@@ -1,5 +1,6 @@
-package com.example.demo.aop.methodSecureAspectJStyle.aspect;
+package com.example.demo.aop.methodSecureAspectJStyle.config;
 
+import com.example.demo.aop.methodSecureAspectJStyle.aspect.AuthorityAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
